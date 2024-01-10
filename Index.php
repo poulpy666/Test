3906dd7c19b2b7bb9b1772985ec3e78da9a0adee
poulpy666 <136070053+poulpy666@users.noreply.github.com>
2024-01-10 21:38:39 +0100

    ajout

--- Index.php
+++ Index.php
@@ -1,28 +1,35 @@
 <?php 
-include 'Personnage.php';
+    include 'Personnage.php';
     // Menu principal du jeu
+    function Partie(){
+        do{
+            function ChoixPerso(){
+                $choixperso = readline("1 Goku 2 Gohan 3 Vegeta: ");
 
-    function ChoixPerso(){
-        $choixperso = readline("1 Goku 2 Gohan 3 Vegeta: ");
+                if($choixperso==1){
+                $goku = new Heros();
+                echo $goku -> getGoku()." a été selectionné\n";
 
-        if($choixperso==1){
-        $goku = new Heros();
-        echo $goku -> getGoku()." a été selectionné\n";
+                }elseif($choixperso==2){
+                    $gohan = new Heros();
+                    echo $gohan -> getGohan()." a été selectionné\n";
 
-        }elseif($choixperso==2){
-            $gohan = new Heros();
-            echo $gohan -> getGohan()." a été selectionné\n";
-
-        }elseif($choixperso==3){
-        $vegeta = new Heros();
-        echo $vegeta -> getVegeta()." a été selectionné\n";
-        }
-        elseif($choixperso != 1 or 2 or 3){
-            echo "Vous devez faire un choix \n";
-        //Faire une loop pour retourner sur le choix
-        }
-    } 
-    echo "Bienvenue dans DragonBall typing fighters Z, choisis un de ces trois personages: \n";
-    ChoixPerso();
-
+                }elseif($choixperso==3){
+                $vegeta = new Heros();
+                echo $vegeta -> getVegeta()." a été selectionné\n";
+                }
+                elseif($choixperso != 1 or 2 or 3 or 4){
+                    echo "Vous devez faire un choix \n";
+                //Faire une loop pour retourner sur le choix
+                }
+                elseif($choixperso==4){
+                    break;
+                }
+            } 
+            echo "Bienvenue dans DragonBall typing fighters Z, choisis un de ces trois personages ou quittez le jeu: \n";
+            ChoixPerso();
+        } while (1==1);
+        
+    }
+    Partie();
 ?>
