d43e3aee9d0c0826b0f310cf48192ee2c8d5b089
Babacar <babacarg836@gmail.com>
2024-01-10 15:11:00 +0100

    ajout

--- Test.php
+++ Test.php
@@ -1,5 +1,58 @@
 <?php
-class essaie{
-    
-} 
+// CLasses Personnage
+ class Personnages{
+    public $nom;
+    public $pv;
+    public $att;
+
+
+    public function construct($n,$p,$a){
+
+        $this->nom=$n;
+        $this->pv=$p;
+        $this->att=$a;
+    }
+    public function getNom(){
+
+        return $this->nom;
+        }
+
+    public function getPv(){
+
+        return $this->pv;
+        } 
+    public function getatt(){
+        return $this->att;
+            }
+ }
+
+//Classes enfants de Personnages
+ class Heros extends Personnages{
+
+    protected $niveau;
+    public function construct($n,$p,$ni){
+
+        parent::construct($n);
+        $this->niveau=$ni;
+    }
+    public function getNiveau(){
+
+        return $this->niveau;
+    }
+
+ }
+ class Mechants extends Personnages{
+
+    parent::construct($n,$ni,$pn,$a);
+    protected $niveau;
+    public function __construct($n,$p,$a,$ni){
+
+        parent::construct($n,$pn,$a);
+        $this->niveau=$ni;
+    }
+    public function getNiveau(){
+
+        return $this->niveau;
+    }
+ }
 ?>
