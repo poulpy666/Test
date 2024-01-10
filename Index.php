cb6e2b26f92af9c039e30e1fbcfbf9dabc0dfe82
poulpy666 <136070053+poulpy666@users.noreply.github.com>
2024-01-10 21:54:51 +0100

    ajout

--- Index.php
+++ Index.php
@@ -6,27 +6,30 @@
         //Selection personnage
         function ChoixPerso(){
             $choixperso = readline("1 Goku 2 Gohan 3 Vegeta: ");
-            switch(){
+            switch{
                     case 1:
                         if($choixperso==1){
                     $goku = new Heros();
                     echo $goku -> getGoku()." a été selectionné\n";
+                    break;
                     }
                     case 2:
                         if($choixperso==2){
                         $gohan = new Heros();
                         echo $gohan -> getGohan()." a été selectionné\n";
-
+                        break;
                     }
                     case 3:
                         if($choixperso==3){
                         $vegeta = new Heros();
                         echo $vegeta -> getVegeta()." a été selectionné\n";
+                        break;
                     }
                     case 4:
                         if($choixperso != 1 or 2 or 3 or 4){
                         echo "Vous devez faire un choix \n";
-                        //Faire une loop pour retourner sur le choix
+                        break;
+
                     }
                     case 5: 
                         if($choixperso==4){
