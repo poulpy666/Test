7692ce84c5ba8541ee0c1f04b56befd104503a5f
poulpy666 <136070053+poulpy666@users.noreply.github.com>
2024-01-10 20:21:37 +0100

    ajout

--- Index.php
+++ Index.php
@@ -1,27 +1,28 @@
 <?php 
 include 'Personnage.php';
     // Menu principal du jeu
-echo "Bienvenue dans DragonBall typing fighters Z, choisis un de ces trois personages: \n";
-ChoixPerso();
-function ChoixPerso(){
-    $choixperso = readline("1 Goku 2 Gohan 3 Vegeta: ");
 
-    if($choixperso==1){
-      $goku = new Heros();
-      echo $goku -> getGoku()." a été selectionné\n";
+    function ChoixPerso(){
+        $choixperso = readline("1 Goku 2 Gohan 3 Vegeta: ");
 
-    }elseif($choixperso==2){
-        $gohan = new Heros();
-        echo $gohan -> getGohan()." a été selectionné\n";
+        if($choixperso==1){
+        $goku = new Heros();
+        echo $goku -> getGoku()." a été selectionné\n";
 
-    }elseif($choixperso==3){
-       $vegeta = new Heros();
-       echo $vegeta -> getVegeta()." a été selectionné\n";
-       }
-    elseif($choixperso != 1 or 2 or 3){
-        echo "Vous devez faire un choix \n";
-       //Faire une loop pour retourner sur le choix
-    }
-} 
+        }elseif($choixperso==2){
+            $gohan = new Heros();
+            echo $gohan -> getGohan()." a été selectionné\n";
+
+        }elseif($choixperso==3){
+        $vegeta = new Heros();
+        echo $vegeta -> getVegeta()." a été selectionné\n";
+        }
+        elseif($choixperso != 1 or 2 or 3){
+            echo "Vous devez faire un choix \n";
+        //Faire une loop pour retourner sur le choix
+        }
+    } 
+    echo "Bienvenue dans DragonBall typing fighters Z, choisis un de ces trois personages: \n";
+    ChoixPerso();
 
 ?>
