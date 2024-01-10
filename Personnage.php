35098b767c7e9a5528d82cc4c8784e3991a5cd33
poulpy666 <136070053+poulpy666@users.noreply.github.com>
2024-01-10 19:09:43 +0100

    ajout

--- Personnage.php
+++ Personnage.php
@@ -51,20 +51,20 @@
     public function getGoku(){
         $this->nom="Goku";
         $this->pv=100;
-        $this->att="Kaioken Smash"// 25 dégats;
+        $this->att="Kaioken Smash";// 25 dégats;
      //niveau == 10 000
     }
     public function getGohan(){
         $this->nom="Gohan";
         $this->pv=100;
-        $this->att="Masenko"//15 dégats;
+        $this->att="Masenko";//15 dégats;
 
         //niveau == 9 000
     }
     public function getVegeta(){
         $this->nom="Vegeta";
         $this->pv=100;
-        $this->att="Final Flash"//20 dégats;
+        $this->att="Final Flash";//20 dégats;
         //niveau == 9 500
     }
 
@@ -77,9 +77,14 @@
         parent::construct($n,$p,$a);
         $this->niveau=$ni;
     }
+
     public function getNiveau(){
 
         return $this->niveau;
+    }
+
+    public function setNiveau(){
+        $this->niveau=$newNiveau;
     }
 
     public function setPV(){
@@ -90,19 +95,19 @@
     public function getFreezer(){
         $this->nom="Freezer";
         $this->pv=100;
-        $this->att="Rayon de la mort"//25 dégats;
+        $this->att="Rayon de la mort";//25 dégats;
         // niveau == 9 990
     }
     public function getCell(){
         $this->nom="Cell";
         $this->pv=100;
-        $this->att="Absorbtion vitale"// 20 dégats;
+        $this->att="Absorbtion vitale";// 20 dégats;
         // niveau == 8 500
     }
     public function getBuu(){
-        $this->nom="Vegeta";
+        $this->nom="Buu";
         $this->pv=100;
-        $this->att="Rayon Chocolat" // 15 dégats;
+        $this->att="Rayon Chocolat"; // 15 dégats;
 
         //niveau == 9500
     }
