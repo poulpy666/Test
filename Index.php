99013614628655b4535ad27e8108614a75a621e5
poulpy666 <136070053+poulpy666@users.noreply.github.com>
2024-01-10 21:49:26 +0100

    ajout

--- Index.php
+++ Index.php
@@ -2,34 +2,46 @@
     include 'Personnage.php';
     // Menu principal du jeu
     function Partie(){
-        do{
-            function ChoixPerso(){
-                $choixperso = readline("1 Goku 2 Gohan 3 Vegeta: ");
+        //do{
+            switch{
+                //Selection personnage
+                function ChoixPerso(){
+                    $choixperso = readline("1 Goku 2 Gohan 3 Vegeta: ");
 
-                if($choixperso==1){
-                $goku = new Heros();
-                echo $goku -> getGoku()." a été selectionné\n";
+                    case 1:
+                        if($choixperso==1){
+                    $goku = new Heros();
+                    echo $goku -> getGoku()." a été selectionné\n";
+                    }
+                    case 2:
+                        if($choixperso==2){
+                        $gohan = new Heros();
+                        echo $gohan -> getGohan()." a été selectionné\n";
 
-                }elseif($choixperso==2){
-                    $gohan = new Heros();
-                    echo $gohan -> getGohan()." a été selectionné\n";
-
-                }elseif($choixperso==3){
-                $vegeta = new Heros();
-                echo $vegeta -> getVegeta()." a été selectionné\n";
-                }
-                elseif($choixperso != 1 or 2 or 3 or 4){
-                    echo "Vous devez faire un choix \n";
-                //Faire une loop pour retourner sur le choix
-                }
-                elseif($choixperso==4){
-                    break;
-                }
+                    }
+                    case 3:
+                        if($choixperso==3){
+                        $vegeta = new Heros();
+                        echo $vegeta -> getVegeta()." a été selectionné\n";
+                    }
+                    case 4:
+                        if($choixperso != 1 or 2 or 3 or 4){
+                        echo "Vous devez faire un choix \n";
+                        //Faire une loop pour retourner sur le choix
+                    }
+                    case 5: 
+                        if($choixperso==4){
+                        break;
+                    }
+                } 
+                echo "Bienvenue dans DragonBall typing fighters Z, choisis un de ces trois personages ou quittez le jeu: \n";
+                //lancement function ChoixPerso
+                ChoixPerso();
             } 
-            echo "Bienvenue dans DragonBall typing fighters Z, choisis un de ces trois personages ou quittez le jeu: \n";
-            ChoixPerso();
-        } while (1==1);
-        
+            function Ennemi(){
+
+            }
+        }
     }
-    Partie();
+    Partie();//la function Partie englobe tout le déroulement des combats et le jeu en globalité
 ?>
