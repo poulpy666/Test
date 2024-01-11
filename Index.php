7fa160b5cf0f507b402adc57f81f5cbcb8c44711
poulpy666 <136070053+poulpy666@users.noreply.github.com>
2024-01-11 14:42:33 +0100

    ajout

--- Index.php
+++ Index.php
@@ -1,11 +1,11 @@
 <?php  //on mets pas l'option quitter au début mais à la fin d'un combat sinon,
-    
+    include 'Personnage.php';
     // Menu principal du jeu
-    function Partie(){//
+    
         do{ 
             //Selection personnage
-            function ChoixPerso(){
-                include 'Personnage.php';
+            function ChoixPerso($goku,$gohan,$vegeta){
+                
                 $choixperso = readline("1 Goku 2 Gohan 3 Vegeta:");
                 // FAIRE UNE BOUCLE POUR LE CHOIX!
                         if($choixperso==1){
@@ -20,35 +20,28 @@
                     else{
                         echo "Vous devez faire un choix \n";
                     }
-                    
-                
             }
             echo "Bienvenue dans DragonBall typing fighters Z, choisis un de ces trois personages: \n";
             //lancement function ChoixPerso
-            ChoixPerso();
-        
-           function Ennemi(){
-                include 'Personnage.php';
+           function Ennemi($freezer,$cell,$buu){
                 $randm=rand(1,3);
                 if($randm==1){
-                    echo"Vous allez affronter: ".affichagenom($freezer); 
+                    echo affichagenom($freezer); 
                 }
                 elseif($randm==2){
-                    echo"Vous allez affronter: ".affichagenom($cell); 
+                    echo affichagenom($cell); 
                 }
                 elseif($randm==3){
-                echo"Vous allez affronter: ".affichagenom($buu); 
+                echo affichagenom($buu); 
                 }
              }
-          Ennemi();
+             ChoixPerso($goku,$gohan,$vegeta);
+             Ennemi($freezer,$cell,$buu);
             $stopouencore= readline("1 pour continuer, 2 pour quitter :");
-        }  while($stopouencore!=2);
-    //fin partie
-    }//on touche pas à cette acollade elle englobe toute la partie    
-
-    Partie();//la function Partie englobe tout le déroulement des combats et le jeu en globalité
-
-    // A FAIRE : changer pv nom ennemis et heros (nomf nomgk etc...);ennemi random et système combat 
+        }while($stopouencore!=2);
+        
+    //on touche pas à cette acollade elle englobe toute la partie    
+    // A FAIRE : ennemi random et système combat 
 
 ?>
 
